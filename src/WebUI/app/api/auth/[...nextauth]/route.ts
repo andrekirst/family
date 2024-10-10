0b901335514b1ae74e620b8fb111f067ee0fe630
André Kirst <github@andrekirst.de>
2024-10-10 11:50:21 +0200

    adding credentialprovider

--- src/WebUI/app/api/auth/[...nextauth]/route.ts
+++ src/WebUI/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,38 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials"
 
 export const authOptions: AuthOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "test", value: "test" },
+                password: { label: "Password", type: "password", value: "test" }
+            },
+            async authorize(credentials, req) {
+              // Add logic here to look up the user from the credentials supplied
+              const user = { id: "1", name: "AK", email: "test@test.de" }
+        
+              if (user) {
+                // Any object returned will be saved in `user` property of the JWT
+                return user
+              } else {
+                // If you return null then an error will be displayed advising the user to check their details.
+                return null
+        
+                // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+              }
+            }
         })
     ],
     callbacks: {
         async jwt({ token, user }) {
-            if(user) {
+            if (user) {
                 token.id = user.id;
             }
 
